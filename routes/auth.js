165e47e96c775031e11edb18be4e2a5b63d493a8
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-01-20 18:35:09 +0100

    Login

--- routes/auth.js
+++ routes/auth.js
@@ -29,15 +29,17 @@ router.post('/register', async (req,res) => {
 });
 
 // Login 
- router.post('/login', (req, res) => {
+ router.post('/login', async (req, res) => {
 
     // Checking if the email exists
-    const emailExist = await User.findOne({email: req.body.email});
-    if(!emailExist) return res.status(400).send('Email or password is wrong');
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Email or password is wrong');
 
     // Checking if the password is correct 
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if(!validPass) return res.status(400).send('Invalid Password')
+
+    res.send('welcome to the Hell !!!')
  });
 
 module.exports = router;
